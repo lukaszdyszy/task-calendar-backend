fd1fa12599fca136934e22062524a0d404caf2ec
lukaszdyszy <mruhol123@gmail.com>
2020-03-27 11:54:04 +0100

    recaptcha v2

--- api/users/register.php
+++ api/users/register.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('../../config/db.php');
+require_once('../../config/secret.php');
 require_once('../../models/user.php');
 
 $data = json_decode(file_get_contents("php://input"));
@@ -11,12 +12,22 @@ $db = new Database();
 $conn = $db->connect();
 
 if(gettype($conn) == 'object'){
-    $user = new User($conn);
-    
-    $user->email = $data->email;
-    $user->password = $data->password;
 
-    $response = $user->register();
+    $siteKey = $data->captcha;
+    $userIP = $_SERVER['REMOTE_ADDR'];
+    $verify = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$siteKey&remoteip=$userIP"));
+
+    if(!$verify->success){
+        $response['error'] = 1;
+        $response['message'] = 'Confirm, you are not a robot.';
+    } else {
+        $user = new User($conn);
+        
+        $user->email = $data->email;
+        $user->password = $data->password;
+
+        $response = $user->register();
+    }
 } else {
     $response = $conn;
 }
